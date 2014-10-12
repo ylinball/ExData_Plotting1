4171ffac006dd09a0f42140ef77e71f95788f35f
ylinball <ylinball@users.noreply.github.com>
2014-10-12 10:18:53 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -33,7 +33,11 @@ concat <- as.character(paste(pset$Date,pset$Time))
 # add datetime column to dataset
 pset$datetime <- strptime(concat,"%d/%m/%Y %H:%M:%S")
 
+png(file="plo1.png")
+
 plot(pset$datetime,pset$Sub_metering_1,type="l", col="black",ylab="Energy sub metering",xlab="")
 lines(pset$datetime, pset$Sub_metering_2, col="red")
 lines(pset$datetime, pset$Sub_metering_3, col="blue")
 legend("topright",lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+dev.off()
